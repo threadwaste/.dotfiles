36f209b77f38f0d2ca8feda5a289095785c2094f
Anthony M <tony@threadwaste.com>
2014-07-22 22:21:24 -0400

    Literally never use backups from deploy and oh god links

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,9 @@
 
 set -e
 
-dotfiles_dir=$(cd `dirname $0` && pwd)
-backup_dir=$HOME/backup-`date +%s`
+cd "$(dirname "$0")"
 
-mkdir -v $backup_dir
-
-for repofile in `find $dotfiles_dir -maxdepth 1 ! -wholename $dotfiles_dir \
-    ! -name .git ! -name .gitignore ! -name .gitmodules \
-    ! -name deploy.sh ! -name README.textile`
-do
-    dotfile=`basename $repofile`
-    if [ -e $HOME/$dotfile ]; then
-        echo -n "moving: " && mv -v $HOME/$dotfile $backup_dir
-    fi
-    echo -n "linking: " && ln -vs $repofile $HOME/$dotfile
-done
+rsync \
+  --exclude ".git/" --exclude ".gitignore" --exclude ".gitmodules" \
+  --exclude "deploy.sh" --exclude "README.textile" \
+  -av . ~
